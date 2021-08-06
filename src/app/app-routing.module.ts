2f04de04546d39150b8113d0f69a5a31b8fb1011
ceoxevello <ak.kumshey@gmail.com>
2021-08-07 00:44:03 +0530

    done with sidenav bar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ChangePassComponent } from './components/change-pass/change-pass.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { EnterOtpComponent } from './components/enter-otp/enter-otp.component';
+import { ForgetPasswordComponent } from './components/forget-password/forget-password.component';
+import { LoginComponent } from './components/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: DashboardComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'forget', component: ForgetPasswordComponent},
+  {path: 'enter-otp', component: EnterOtpComponent},
+  {path: 'change-password', component: ChangePassComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+export const routingComponents = [LoginComponent, ForgetPasswordComponent, EnterOtpComponent, ChangePassComponent, DashboardComponent ]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { ForgetPasswordComponent } from './components/forget-password/forget-password.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NbThemeModule, NbLayoutModule } from '@nebular/theme';
+import { NbEvaIconsModule } from '@nebular/eva-icons';
+import { EnterOtpComponent } from './components/enter-otp/enter-otp.component';
+import { ChangePassComponent } from './components/change-pass/change-pass.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { SidenavComponent } from './components/sidenav/sidenav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    ForgetPasswordComponent,
+    EnterOtpComponent,
+    ChangePassComponent,
+    DashboardComponent,
+    SidenavComponent,
+    SidenavComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    NbThemeModule.forRoot({ name: 'default' }),
+    NbLayoutModule,
+    NbEvaIconsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
